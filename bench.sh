1d4ebd561c61ac5be971f4d82ddf6fd98aa84adb
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-07-23 11:36:58 +0900

    fix bugs

--- bench.sh
+++ bench.sh
@@ -2,9 +2,9 @@
 (
   cd ~/isucon9-qualify
 
-  sudo truncate --size 0 /var/log/nginx/access.log
-  sudo truncate --size 0 /var/log/mysql/mysql-slow.log
-  sudo systemclt restart isucari.golang.service && echo "restart isucari.golang.service"
+  sudo truncate --size 0 /var/log/nginx/access.log && echo "clear access.log"
+  sudo truncate --size 0 /var/log/mysql/mysql-slow.log && echo "clear mysql-slow.log"
+  sudo systemctl restart isucari.golang.service && echo "restart isucari.golang.service"
   sudo systemctl stop payment.service shipment.service && echo "stop payment.service shipment.service"
   ./bin/benchmarker -target-url http://127.0.0.1
 )
